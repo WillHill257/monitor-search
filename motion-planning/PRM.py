e0e5d1fb2396e304c1bb00ff8c541db3f561dcc0
Iretonsama <43218442+Iretonsama@users.noreply.github.com>
2022-05-29 15:11:27 +0200

    completed PRM framework (not tested)

--- motion-planning/PRM.py
+++ motion-planning/PRM.py
@@ -3,8 +3,118 @@
 - should not be run in isolation, should be imported wherever needed
 """
 
+from map import Map
+from graph import *
+import random
+from typing import List
+
 
 class PRM:
     # initialise
-    def __init__(self) -> None:
-        pass
+    def __init__(self, adjacency_radius, step_size, occupancy_threshold, robot_radius) -> None:
+        self.worldmap = Map(step_size, occupancy_threshold, robot_radius)
+        self.roadmap = Graph()
+        self.start = None
+        self.target = None
+
+        # radius for adjacencies of nodes
+        self.radius = adjacency_radius
+        self.ROADMAP_MAX_ADDITIONAL_SAMPLES = 100
+        self.ROADMAP_ADDITIONAL_SAMPLE_SIZE = 100
+
+    # updates the map of the world/environment
+
+    def updateMap(self, occupancyGrid):
+        self.worldmap.update(occupancyGrid)
+
+    # sample a random point with in the bounds of the world map
+    def samplePoint(self):
+        x = random.randint(0, self.worldmap.width-1)
+        y = random.randint(0, self.worldmap.height-1)
+
+        return (x, y)
+
+    # check if 2 nodes are adjancent and valid neighbours
+    def performNodeAdjacencyAddition(self, node1, node2):
+        # check for radius
+        if(node1.distance(node2) > self.radius):
+            return
+
+        # check if a node is visible to another
+        if(not self.worldmap.isVisible(node1, node2)):
+            return
+
+        # add nodes as neighbours of each other
+        node1.addAdjacency(node2)
+        node2.addAdjacency(node1)
+
+    # expand with roadmap with "sampleCount" number of random samples
+    def expandRoadmap(self, sampleCount):
+
+        for _ in range(sampleCount):
+            x, y = self.samplePoint()
+
+            if(self.worldmap.isFree(x, y)):
+                new_node = Node(x, y)
+                self.roadmap.addNode(new_node)
+                for node in self.roadmap.nodes:
+                    if(node != new_node):
+                        self.performNodeAdjacencyAddition(node, new_node)
+
+    # set the start coordinate
+    def setStart(self, start: Node) -> None:
+        # remove the current start from the graph
+        if self.start != None:
+            self.graph.removeNode(self.start)
+
+        # set the start node
+        self.start = start
+
+        # check the start node for valid adjacencies
+        # loop through the graphs nodes
+        for node in self.graph.nodes:
+            # check if valid and adjcent node
+            self.performNodeAdjacencyAddition(self.start, node)
+
+        # add the start to the graph
+        self.graph.addNode(self.start)
+
+    # set the target coordinate
+    def setTarget(self, target: Node) -> None:
+        # remove the current target from the graph
+        if self.target != None:
+            self.graph.removeNode(self.target)
+
+        # set the target node
+        self.target = target
+
+        # check the target node for valid adjacencies
+        # loop through the graphs nodes
+        for node in self.graph.nodes:
+            # check if valid and adjacent node
+            self.performNodeAdjacencyAddition(self.target, node)
+
+        # add the target to the graph
+        self.graph.addNode(self.target)
+
+    # find a path
+    def findPath(self) -> List[str]:
+        # try and find a path
+        path_found, path = self.graph.astar(self.start, self.target)
+
+        # if a path doesn't exist, sample more points and try again
+        # do for a maximum number of times before returning no path
+        count = 0
+        while (not path_found) and (
+            count < self.ROADMAP_MAX_ADDITIONAL_SAMPLES
+        ):
+
+            # expand the roadmap
+            self.expandRoadmap(self.ROADMAP_ADDITIONAL_SAMPLE_SIZE)
+
+            # check for a path
+            path = self.graph.astar(self.start, self.target)
+
+            count += 1
+
+        return path
--- motion-planning/map.py
+++ motion-planning/map.py
@@ -3,14 +3,27 @@
 - Includes transformation functions so that our PRM view is constant
 """
 
+from nav_msgs.msg import OccupancyGrid, MapMetaData
+from geometry_msgs.msg import Pose
+import numpy as np
+from skimage.morphology import disk, binary_dilation
 
-class OccupancyGrid:
-    def __init__(self) -> None:
+
+class Map:
+    def __init__(self, step_size, occupancy_threshold, robot_radius) -> None:
         # define the map, which is a 1D array of the specified height and width
         # map is ROW-MAJOR
-        self.map = None
+        self.data = []
         self.height = 0
         self.width = 0
+        self.origin = None
+
+        # visibility ray casting step size
+        self.step_size = step_size
+        # the threshold probability for a occupancy cell to be considered as free
+        self.threshold = occupancy_threshold
+        # the radius of the robot itself
+        self.robot_radius = robot_radius
 
     # return the element given a 2D index
     def at(self, x: int, y: int):
@@ -19,3 +32,51 @@ class OccupancyGrid:
     # return the element given a 1D index - OVERLOADED
     def at(self, index: int):
         return self.map[index]
+
+    # updates the map with occupancy grid
+    def updateMap(self, occupancyGrid) -> None:
+
+        # convert the occupancy grid into a binary image
+        # this is useful for dilation purposes
+        self.data = np.array(occupancyGrid.data)
+        self.data = np.abs(self.data)
+        self.data = self.data > self.threshold
+
+        # dilate the binary grid to account for size of robot
+        SE = disk(self.robot_radius)
+        self.data = binary_dilation(self.data, SE)
+
+        self.height = occupancyGrid.info.height
+        self.width = occupancyGrid.info.width
+        self.origin = occupancyGrid.info.origin
+
+    # checks if a given coord is free, i.e. no obstacles and known
+    def isFree(self, x, y):
+        # TODO: tune this
+        return self.at(x, y) == 0
+
+    def isVisible(self, node1, node2):
+        dist = node1.distance(node2)
+
+        # use a ray to check for a intersection between the nodes
+        n1 = np.array([node1.x, node1.y])
+        n2 = np.array([node2.x, node2.y])
+
+        # ray march from the first point to the second
+        ro = n1
+        rd = n2 - n1
+        rd = rd/np.sum(rd)  # normalise ray direction
+
+        dist_travelled = 0
+        pos = ro
+
+        # ray marching loop
+        while(dist_travelled < dist):
+            pos += rd * self.step_size
+
+            if(not self.isFree(int(pos[0]), int(pos[1]))):
+                return False
+
+            dist_travelled += self.step_size
+
+        return True
